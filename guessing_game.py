93e6dd0ee3f0017b12470fc2a11a313953d99e31
lambinh <binh.lam@global.ntt>
2023-02-12 23:20:16 +1100

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,12 @@
 import random
+import time
 
 def guess_number_game():
   # Generate a random number between 1 and 100
   number = random.randint(1, 100)
+
+  # Start timing the game
+  start_time = time.time()
 
   # Ask the user to guess the number
   guess = int(input("Guess a number between 1 and 100: "))
@@ -14,9 +18,9 @@ def guess_number_game():
   while guess != number:
     # Give the user a hint
     if guess < number:
-      print("It's too low! Try again.")
+      print("Too low! Try again.")
     else:
-      print("It's too high! Try again.")
+      print("Too high! Try again.")
 
     # Ask the user to guess again
     guess = int(input("Guess a number between 1 and 100: "))
@@ -24,8 +28,12 @@ def guess_number_game():
     # Increment the number of attempts
     attempts += 1
 
+  # End timing the game
+  end_time = time.time()
+
   # The user has guessed the number
   print("Congratulations, You've got it in", attempts, "attempts! The number was", number)
+  print("The game took", end_time - start_time, "seconds.")
 
 # Start the game
 guess_number_game()
